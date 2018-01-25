ac566dfa3977093084cd09b743a07a576fe55653
Simon C. Wu <simon.c.wu@gmail.com>
2018-01-25 19:00:04 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,4 +37,16 @@ class TicTacToe
   def valid_move?(index)
     index.between?(0,8) && !position_taken?(index)
   end
+  
+  def turn
+    loop do
+      puts "Please enter 1-9:"
+      user_input = gets.strip
+      index = input_to_index(user_input)
+      if valid_move?(index)
+        move(index, current_player(board))
+        break
+      end
+    end
+  end
 end
