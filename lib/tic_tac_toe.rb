8f4008510613fa7c85d70769a1b38ac1ef59bf42
Simon C. Wu <simon.c.wu@gmail.com>
2018-01-25 19:00:47 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -44,9 +44,20 @@ class TicTacToe
       user_input = gets.strip
       index = input_to_index(user_input)
       if valid_move?(index)
-        move(index, current_player(board))
+        move(index, current_player
         break
       end
     end
   end
+  
+  def turn_count(board)
+  counter = 0
+  board.each{|cell| counter += 1 if cell == "X" || cell == "O"}
+  counter
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
 end
