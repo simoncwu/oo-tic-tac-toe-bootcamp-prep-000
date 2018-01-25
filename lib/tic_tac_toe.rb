c6ef0c46c3bbe7c5750c3770370849de48990fb5
Simon C. Wu <simon.c.wu@gmail.com>
2018-01-25 18:47:33 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -33,4 +33,8 @@ class TicTacToe
   def position_taken?(index)
     !(@board[index].nil? || @board[index] == " " || @board[index] == "")
   end
+  
+  def valid_move?(index)
+    index.between?(0,8) && !position_taken?(@board, index)
+  end
 end
